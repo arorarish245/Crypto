bc4ca34c5769e7c269bce0745cfbd7fda7bbebed
Rishita Arora <rishitaarora245@gmail.com>
2024-08-04 13:15:59 +0530

    first commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Footer.css"
+const Footer = () => {
+  return (
+    <div>
+      <p>Copyright @ 2024, Crypto-All Right Reserved</p>
+    </div>
+  )
+}
+
+export default Footer
